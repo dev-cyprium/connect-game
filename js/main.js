6164c4043fd699412242c3dd298121716980a864
Stefan Kupresak <stefan_vg@hotmail.com>
2017-06-18 21:41:33 +0200

    Add gameloop to the game

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ var Game = function() {
 }
 
 Game.prototype = (function() {
+	var timeAtLastFrame = new Date().getTime();
+	var idealTimePerFrame = 1000 / 30;
+	var leftover = 0.0;
+	
 	var initiateWebGL = function(canvas) {
 		gl = null;
 		gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
@@ -16,13 +20,32 @@ Game.prototype = (function() {
 		return gl;
 	}
 	
+	var tick = function() {
+		var timeAtThisFrame = new Date().getTime();
+		var timeSinceLastDoLogic = (timeAtThisFrame - timeAtLastFrame) + leftover;
+		var catchUpFrameCount = Math.floor(timeSinceLastDoLogic / idealTimePerFrame);
+		
+		for(var i=0; i < catchUpFrameCount; i++) {
+			// Do logic
+		}
+		
+		// Render scene
+		
+		leftover = timeSinceLastDoLogic - (catchUpFrameCount * idealTimePerFrame);
+		timeAtLastFrame = timeAtThisFrame;
+	}
+	
 	return {
 		constructor: Game,
-		
 		init: function( canvas ) {
-			gl = initiateWebGL(canvas);
+			this.gl = initiateWebGL(canvas);
 			gl.clearColor(0.0, 0.0, 0.0, 1.0)
 			gl.clear(gl.COLOR_BUFFER_BIT)
+		},
+		run: function() {
+			
+			
+			setInterval(tick, idealTimePerFrame)	
 		}
 	}
 })();
