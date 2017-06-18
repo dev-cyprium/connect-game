daf32c94584c18adab41414fa1706eba120be309
Stefan Kupresak <stefan_vg@hotmail.com>
2017-06-18 21:33:26 +0200

    Add initialization code

--- js/main.js
+++ js/main.js
@@ -2,11 +2,27 @@
 	Game instance, class containing the game
 */
 var Game = function() {
-	this.stage = new createjs.Stage('gameCanvas');
-	this.done = false;
 }
 
-Game.prototype.run = function() {
-	createjs.Ticker.setFPS(60);
-	createjs.Ticker.addEventListener("tick", this.stage)
-}
+Game.prototype = (function() {
+	var initiateWebGL = function(canvas) {
+		gl = null;
+		gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+			
+		if (!gl) {
+			alert('Unable to initialize WebGL. Your browser may not support it');
+		}
+			
+		return gl;
+	}
+	
+	return {
+		constructor: Game,
+		
+		init: function( canvas ) {
+			gl = initiateWebGL(canvas);
+			gl.clearColor(0.0, 0.0, 0.0, 1.0)
+			gl.clear(gl.COLOR_BUFFER_BIT)
+		}
+	}
+})();
