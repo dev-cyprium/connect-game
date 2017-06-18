daa2a9f53b7137b7948b66bd9b9cc3b5e2f3cfdc
Stefan Kupresak <stefan_vg@hotmail.com>
2017-06-18 22:23:02 +0200

    Add phaser to the project

--- js/game_objects.js
+++ js/game_objects.js
@@ -5,3 +5,16 @@ Grid.prototype.update = function() {
 Grid.prototype.render = function(graphics) {
 	
 }
+
+var BlankTile = function(x, y, scene) {
+	this.x = x;
+	this.y = y;
+	this.scene = scene;
+	
+	this.width  = BlankTile.SIZE;
+	this.height = BlankTile.SIZE;
+	this.color  = 0xFF00FF;
+}
+BlankTile.render = function(gl) {
+}
+BlankTile.SIZE = 50;
