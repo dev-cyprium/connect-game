407a9514c742a24b4c18a80cb94bf9a9d66ee805
Stefan Kupresak <stefan_vg@hotmail.com>
2017-06-18 22:53:46 +0200

    Add implementation for Tile

--- js/game_objects.js
+++ js/game_objects.js
@@ -1,5 +1,4 @@
 var Grid = function() {
-	this.rect = new Phaser.Rectangle(10, 10, 100, 100);
 }
 Grid.prototype.update = function() {
 	
@@ -15,8 +14,25 @@ var BlankTile = function(x, y, scene) {
 	
 	this.width  = BlankTile.SIZE;
 	this.height = BlankTile.SIZE;
-	this.color  = 0xFF00FF;
+	this.color  = '#555555';
+	this.rect   = Phaser.Rectangle(this.x, this.y, this.width, this.height);
+	this.active = true
+	this.hoverColor  = '#aaaaaa';
+	this.normalColor = '#000000';
+	this.ocupied = false; 
 }
-BlankTile.render = function(gl) {
+BlankTile.render = function(game) {
+	if(this.active) {
+		this.normalColor = this.hoverColor;
+	} else {
+		this.normalColor = '#000000';
+	}
+	game.debug.geom(this.color, this.rect)
+	game.debug.geom(this.rect, this.normalColor, Phaser.Rectangle(
+		this.x + 2,
+		this.y + 2,
+		this.width - 4,
+		this.height - 4
+	));
 }
 BlankTile.SIZE = 50;
