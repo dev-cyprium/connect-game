5020fa766e39bb59362687205aef54ca72d02440
Stefan Kupresak <stefan_vg@hotmail.com>
2017-06-18 22:27:56 +0200

    Add phaser library

--- js/main.js
+++ js/main.js
@@ -5,54 +5,32 @@ var Game = function() {
 }
 
 Game.prototype = (function() {
-	var timeAtLastFrame = new Date().getTime();
-	var idealTimePerFrame = 1000 / 30;
-	var leftover = 0.0;
-	var sceneManager = new SceneManager();
-	var gl;
-	
-	var initiateWebGL = function(canvas) {
-		gl = null;
-		gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
-			
-		if (!gl) {
-			alert('Unable to initialize WebGL. Your browser may not support it');
-		}
-			
-		return gl;
+	var game;
+	var gameOptions = {
+		width: 800,
+		height: 600
 	}
 	
-	/*
-		Game loop which renders and updates the game
-	*/
-	var tick = function(gl) {
-		var timeAtThisFrame = new Date().getTime();
-		var timeSinceLastDoLogic = (timeAtThisFrame - timeAtLastFrame) + leftover;
-		var catchUpFrameCount = Math.floor(timeSinceLastDoLogic / idealTimePerFrame);
+	var _preload = function() {
 		
-		var active_scene = sceneManager.activeScene();
-		// TODO: dispatch events
+	}
+	
+	var _create = function() {
 		
-		for(var i=0; i < catchUpFrameCount; i++) {
-			active_scene.update();
-		}
+	}
+	
+	var _update = function() {
 		
-		gl.clearColor(0.0, 0.0, 0.0, 1.0);
-		gl.clear(gl.COLOR_BUFFER_BIT);
-		active_scene.render(gl);
-		
-		
-		leftover = timeSinceLastDoLogic - (catchUpFrameCount * idealTimePerFrame);
-		timeAtLastFrame = timeAtThisFrame;
 	}
 	
 	return {
 		constructor: Game,
 		init: function( canvas ) {
-			gl = initiateWebGL(canvas);
-		},
-		run: function() {
-			setInterval( function() { tick(gl) }, idealTimePerFrame);
+			game = new Phaser.Game(gameOptions.width, gameOptions.height, Phaser.AUTO, '', {
+				preload: _preload,
+				create: _create,
+				update: _update
+			});
 		}
 	}
 })();
